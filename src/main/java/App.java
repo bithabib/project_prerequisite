fd4ae4a2907e2726039c870b5c449382120402fd
bithabib <habib.usa2014@gmail.com>
2025-09-09 02:09:21 +0900

    Task is completed

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("HelloWorld message: " + bean1.getMessage());
+        System.out.println("HelloWorld beans are the same: " + (bean1 == bean2));
+        System.out.println("Cat beans are the same: " + (cat1 == cat2));
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,30 +1,29 @@
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = AppConfig.class)
-//public class AppTest {
-//
-//    @Autowired
-//    private ApplicationContext applicationContext;
-//
-//    @Test
-//    public void mainTest() {
-//        HelloWorld tree =
-//                applicationContext.getBean(HelloWorld.class);
-//        HelloWorld leaf =
-//                applicationContext.getBean(HelloWorld.class);
-//
-//        Cat one = applicationContext.getBean(Cat.class);
-//        Cat two = applicationContext.getBean(Cat.class);
-//
-//        Assert.assertSame("Test failed, incorrect HelloWorld bean configuration", tree, leaf);
-//        Assert.assertNotSame("Test failed, incorrect Cat bin configuration", one, two);
-//    }
-//}
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppTest {
+
+    @Test
+    public void testHelloWorldSingleton() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld bean1 = (HelloWorld) context.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) context.getBean("helloworld");
+
+        // Same reference → true (singleton scope by default)
+        Assert.assertTrue(bean1 == bean2);
+    }
+
+    @Test
+    public void testCatPrototype() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Cat cat1 = (Cat) context.getBean("cat");
+        Cat cat2 = (Cat) context.getBean("cat");
+
+        // Different reference → false (prototype scope)
+        Assert.assertFalse(cat1 == cat2);
+    }
+}

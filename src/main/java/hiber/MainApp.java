efa79f99c6b21eeabe0106876f66dac56a100daa
bithabib <habib.usa2014@gmail.com>
2025-09-10 23:21:07 +0900

    Update the solution

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,25 +11,25 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+       AnnotationConfigApplicationContext context =
+               new AnnotationConfigApplicationContext(AppConfig.class);
 
-      UserService userService = context.getBean(UserService.class);
+       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+       User user1 = new User("John", "Doe", "john@example.com", new Car("BMW", 5));
+       User user2 = new User("Alice", "Smith", "alice@example.com", new Car("Audi", 3));
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+       userService.add(user1);
+       userService.add(user2);
 
-      context.close();
+       List<User> users = userService.listUsers();
+       for (User user : users) {
+           System.out.println(user + " owns " + user.getCar());
+       }
+
+       User owner = userService.getUserByCar("BMW", 5);
+       System.out.println("Owner of BMW 5: " + owner.getFirstName());
+
+       context.close();
    }
 }
